a26429e1fd5b34069ce6f42f898d124caafa4de9
Adam Swanson <adam.swanson.98@gmail.com>
2024-05-27 12:52:29 -0400

    Reinstall Storybook

---
+++ src/stories/Display.stories.tsx
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { Display } from "../components/Display";
+
+const meta = {
+  title: "Example/Display",
+  component: Display,
+  argTypes: {
+    color: { control: "color" },
+    height: { control: "number" },
+    value: {
+      control: "text",
+    },
+    count: {
+      control: "number",
+    },
+    backgroundColor: {
+      control: "color",
+    },
+  },
+} satisfies Meta<typeof Display>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    color: "red",
+    height: 250,
+    value: "0",
+    count: 2,
+    backgroundColor: "black",
+    skew: false,
+  },
+};
